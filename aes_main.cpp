c2509a3d99a87b24b13464e4a748167975f26957
Josh Daymude <joshdaymude@gmail.com>
2018-11-14 15:23:29 -0700

    + Updated the main driver to instantiate the mode of operation.

--- aes_main.cpp
+++ aes_main.cpp
@@ -4,8 +4,11 @@
 #include <iostream>
 #include <string>
 
-// #include "aes.h"
+#include "aes.h"
 #include "algebra.h"
+#include "cbcmode.h"
+#include "ctrmode.h"
+#include "ofbmode.h"
 
 int main(int argc, char* argv[]) {
   // Argument checking.
@@ -21,8 +24,19 @@ int main(int argc, char* argv[]) {
   std::string modeStr = argv[3];
 
   // Instantiate the AES and mode of operation objects.
-  // AES * aes = new AES(nk);
-  // TODO: instantiate ModeOfOp after deciding how inheritance will work.
+  AES * aes = new AES(nk);
+  ModeOfOp mode;
+  if (modeStr.equals("cbc")) {
+    mode = CBCMode(aes, 16);
+  } else if (modeStr.equals("ofb")) {
+    mode = OFBMode(aes, 16);
+  } else if (modeStr.equals("ctr")) {
+    mode = CTRMode(aes, 16);
+  } else {
+    std::cout << "[mode of op] must be one of \'cbc\', \'ofb\', or \'ctr\'."
+              << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
   // TODO: encrypt the plaintext according to the mode of operation.
   // TODO: output the ciphertext.
